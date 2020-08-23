01d9888d334463450b118aa232a963b0fcdc0a69
This PC <devvvrats@gmail.com>
2020-08-23 21:51:50 +0530

    Create constructor of Student.java class.

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -14,4 +14,42 @@ public class Student {
     private int numberOfBooksIssuedByStudent;
     private Book[] allBooksIssuedByStudent;
 
+    /**
+     * Sets all fields with given values.
+     *
+     * @param nameOfStudent                Name of the student.
+     * @param rollNumberOfStudent          Roll number of the respective student.
+     * @param numberOfBooksIssuedByStudent Total number of books issued by particular Student.
+     * @param allBooksIssuedByStudent      Details of all the books issued by particular student.
+     */
+    public Student(String nameOfStudent, long rollNumberOfStudent, int numberOfBooksIssuedByStudent, Book[] allBooksIssuedByStudent) {
+        this.nameOfStudent = nameOfStudent;
+        this.rollNumberOfStudent = rollNumberOfStudent;
+        this.numberOfBooksIssuedByStudent = numberOfBooksIssuedByStudent;
+        this.allBooksIssuedByStudent = allBooksIssuedByStudent;
+    }
+
+    /**
+     * Set name of the student to default and initialise array with default Book constructor.
+     */
+    public Student() {
+        this.nameOfStudent = "NOT AVAILABLE";
+        allBooksIssuedByStudent = new Book[MAXIMUM_NUMBER_OF_BOOKS_CAN_BE_ISSUED];
+        for (int index = 0; index < allBooksIssuedByStudent.length; index++) {
+            allBooksIssuedByStudent[index] = new Book();
+        }
+    }
+
+    /**
+     * Set name of the student to given value and initialise array with default Book constructor.
+     *
+     * @param nameOfStudent Name of the student.
+     */
+    public Student(String nameOfStudent) {
+        this.nameOfStudent = nameOfStudent;
+        allBooksIssuedByStudent = new Book[MAXIMUM_NUMBER_OF_BOOKS_CAN_BE_ISSUED];
+        for (int index = 0; index < allBooksIssuedByStudent.length; index++) {
+            allBooksIssuedByStudent[index] = new Book();
+        }
+    }
 }
