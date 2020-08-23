39ef39adae50d8da4c75691e9de72c0bed7b9996
This PC <devvvrats@gmail.com>
2020-08-23 20:03:59 +0530

    Add constructor method to class Book.java.

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -11,4 +11,36 @@ public class Book {
     private String authorOfTheBook;
     private String bookISBNCode;
 
+    /**
+     * Set the name of the book and name of the author of the book.
+     *
+     * @param nameOfBook      Name of the Book.
+     * @param authorOfTheBook Name of the author of the respective book.
+     */
+    public Book(String nameOfBook, String authorOfTheBook) {
+        this.nameOfBook = nameOfBook;
+        this.authorOfTheBook = authorOfTheBook;
+    }
+
+    /**
+     * Set the ISBN code of the book.
+     *
+     * @param bookISBNCode ISBN code of the book.
+     */
+    public Book(String bookISBNCode) {
+        if (bookISBNCode.length() == 13) {
+            this.bookISBNCode = bookISBNCode;
+        } else {
+            System.out.println("Please enter correct ISBN code");
+        }
+    }
+
+    /**
+     * Initialise all fields with default value.
+     */
+    public Book() {
+        this.nameOfBook = "NOT AVAILABLE";
+        this.authorOfTheBook = "NOT AVAILABLE";
+        this.bookISBNCode = "NOT AVAILABLE";
+    }
 }
