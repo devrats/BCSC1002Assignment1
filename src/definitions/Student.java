d6a7771c9067b19ad793d6fc57c9789b94ab7115
This PC <devvvrats@gmail.com>
2020-08-23 22:06:35 +0530

    Create getters and setters methods  of Student.java class.

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -52,4 +52,85 @@ public class Student {
             allBooksIssuedByStudent[index] = new Book();
         }
     }
+
+    /**
+     * Returns details of all books issued by particular student.
+     *
+     * @return Details of all books issued by particular student.
+     */
+    public Book[] getAllBooksIssuedByStudent() {
+        return allBooksIssuedByStudent;
+    }
+
+    /**
+     * Sets details of all books issued by particular student.
+     *
+     * @param allBooksIssuedByStudent Details of books issued by particular student.
+     */
+    public void setAllBooksIssuedByStudent(Book[] allBooksIssuedByStudent) {
+        this.allBooksIssuedByStudent = allBooksIssuedByStudent;
+    }
+
+    /**
+     * Return total number of books issued currently to particular student.
+     *
+     * @return Total number of books issued currently to particular student.
+     */
+    public int getNumberOfBooksIssuedByStudent() {
+        return numberOfBooksIssuedByStudent;
+    }
+
+    /**
+     * Set total number of books issued currently to particular student.
+     *
+     * @param numberOfBooksIssuedByStudent Total number of issued books.
+     */
+    public void setNumberOfBooksIssuedByStudent(int numberOfBooksIssuedByStudent) {
+        this.numberOfBooksIssuedByStudent = numberOfBooksIssuedByStudent;
+    }
+
+    /**
+     * Return roll number of the particular student.
+     *
+     * @return Roll number of the particular student.
+     */
+    public long getRollNumberOfStudent() {
+        return rollNumberOfStudent;
+    }
+
+    /**
+     * Set roll number of the particular student.
+     *
+     * @param rollNumberOfStudent Roll number of particular student.
+     */
+    public void setRollNumberOfStudent(long rollNumberOfStudent) {
+        this.rollNumberOfStudent = rollNumberOfStudent;
+    }
+
+    /**
+     * Return the name of the student.
+     *
+     * @return Name of the student.
+     */
+    public String getNameOfStudent() {
+        return nameOfStudent;
+    }
+
+    /**
+     * Set the name of the student.
+     *
+     * @param nameOfStudent Name of the student.
+     */
+    public void setNameOfStudent(String nameOfStudent) {
+        this.nameOfStudent = nameOfStudent;
+    }
+
+    /**
+     * Return maximum number of books a student can issue at one time.
+     *
+     * @return Maximum number of books a student can issue at one time.
+     */
+    public int getMAXIMUM_NUMBER_OF_BOOKS_CAN_BE_ISSUED() {
+        return MAXIMUM_NUMBER_OF_BOOKS_CAN_BE_ISSUED;
+    }
 }
