01dc1baeabcd4860ec36fee8a4d92ade7886f96a
This PC <devvvrats@gmail.com>
2020-08-24 12:04:01 +0530

    Add final fields for FrontDesk.java class.

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -7,7 +7,16 @@
 package execution;
 
 public class FrontDesk {
+
+    private static final int SHOW_ALL_BOK = 1;
+    private static final int ISSUE_BOK = 2;
+    private static final int RETURN_BOOK = 3;
+    private static final int EXIT = 4;
+    private static final int ADD_BOOK = 5;
+    private static final int REMOVE_BOOK = 6;
+
     public static void main(String[] args) {
 
     }
+
 }
