29a84abc6fc014b2ec256310a1576b01465570c4
This PC <devvvrats@gmail.com>
2020-08-24 12:44:48 +0530

    Add menu for student for FrontDesk.java class.

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -6,17 +6,58 @@
  * */
 package execution;
 
+import definitions.Library;
+import definitions.Student;
+
+import java.util.Scanner;
+
 public class FrontDesk {
 
-    private static final int SHOW_ALL_BOK = 1;
-    private static final int ISSUE_BOK = 2;
-    private static final int RETURN_BOOK = 3;
-    private static final int EXIT = 4;
-    private static final int ADD_BOOK = 5;
-    private static final int REMOVE_BOOK = 6;
+    private static final int SHOW_ALL_BOOK = 3;
+    private static final int ISSUE_BOOK = 1;
+    private static final int RETURN_BOOK = 2;
+    private static final int EXIT = 0;
+    private static final int ADD_BOOK = 1;
+    private static final int REMOVE_BOOK = 2;
 
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        int clientInput;
+        Library library = new Library();
+        System.out.println("You are a librarian or a student: ");
+        if (input.nextLine().equalsIgnoreCase("student")) {
+            Student student = new Student();
+            do {
+                System.out.println("-=-=-=-=Welcome To Front End Desk=-=-=-=-");
+                System.out.println("How may I help today?");
+                System.out.println("1. Issue a new book for me: ");
+                System.out.println("2. Return a previously issued book for me: ");
+                System.out.println("3. Show me all my issued books: ");
+                System.out.println("0. Exit: ");
+                System.out.println("Enter your choice (0..3): ");
+                clientInput = input.nextInt();
+                switch (clientInput) {
+                    case ISSUE_BOOK -> {
+                        System.out.println("Enter the name of the Book you want to issue: ");
+                        input.nextLine();
+                        String bookName = input.nextLine();
+                        System.out.println("Enter the author of the " + bookName);
+                        String authorName = input.nextLine();
+                        student.doIssue(bookName, authorName, library);
+                    }
+                    case RETURN_BOOK -> {
+                        System.out.println("Enter the name of the Book you want to return: ");
+                        input.nextLine();
+                        String bookName = input.nextLine();
+                        System.out.println("Enter the author of the " + bookName);
+                        String authorName = input.nextLine();
+                        student.doReturn(bookName, authorName, library);
+                    }
+                    case SHOW_ALL_BOOK -> student.listBooksIssuedToStudent();
+                    default -> System.out.println("WRONG CHOICE");
+                }
+            } while (clientInput != EXIT);
+        }
     }
 
 }
